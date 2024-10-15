3a90e219665cc0be8b117fe0d32bfe3db7f2240f
adilsygmetiv <adilummersygmetiv@gmail.com>
2024-10-15 15:22:29 +0530

    setted the dba nd api

--- api/index.js
+++ api/index.js
@@ -1,5 +1,22 @@
-import express from 'express';
+import express from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+import userRouter from "./routers/user.route.js";
 
 const app = express();
+dotenv.config();
 
-app.listen(3000,()=>(console.log('Server is running on port 3000!!!')));
+const mongo_url = process.env.MONGO_DB;
+
+mongoose
+  .connect(mongo_url)
+  .then(() => {
+    console.log("database connected successfully");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use("/api/user", userRouter);
+
+app.listen(3000, () => console.log("Server is running on port 3000!!!"));
